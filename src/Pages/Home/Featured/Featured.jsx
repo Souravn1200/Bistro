902753e4a32a2743c0a8818934ef2b6328fa6a60
Souravn1200 <souravn1200@gmail.com>
2023-11-12 12:55:38 +0600

    Made featured section paralax and fixed the bg overlay and bg positon to cover

--- src/Pages/Home/Featured/Featured.jsx
+++ src/Pages/Home/Featured/Featured.jsx
@@ -7,9 +7,11 @@ import './Featured.css'
 
 const Featured = () => {
     return (
-        <div className='featuredItem text-white'>
-            <Pagetitle subHeading="Should try" Heading="featured"></Pagetitle>
-            <section className='md:flex justify-center items-center pb-20 pt-12 px-36 '>
+        <div className='featuredItem bg-fixed bg-cover text-white'>
+
+<div className='featured-overlay'>
+<Pagetitle subHeading="Should try" Heading="featured"></Pagetitle>
+            <section className=' md:flex justify-center items-center pb-20 pt-12 px-36 '>
                 <div>
                     <img src={featuredImage} alt="" />
                 </div>
@@ -18,9 +20,12 @@ const Featured = () => {
                     <p>Where can i get some</p>
                     <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam sequi dolores velit fugiat. Qui exercitationem dolor ex, soluta ab unde, quibusdam rerum corrupti quos veniam inventore libero? Sed, iste nisi!
                     Cupiditate praesentium culpa voluptatibus id accusantium, odit recusandae enim. Repudiandae laboriosam ea, deleniti adipisci omnis accusamus eaque, culpa fuga modi, molestias dolorem? Sed, optio veritatis dicta beatae numquam eum ad!</p>
-                    <button className="btn btn-outline">Order Now</button>
+                    <button className="btn btn-outline text-white w-1/3 ml-[30%] mt-6 border-0 border-b-4">Order Now</button>
                 </div>
             </section>
+
+</div>
+
         </div>
     );
 };
