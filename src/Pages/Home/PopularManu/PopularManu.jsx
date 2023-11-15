2270e16d8fbf83c78b694708bc09ed63147c07b8
Souravn1200 <souravn1200@gmail.com>
2023-11-15 12:48:46 +0600

    Made a custom hook for fetching json data and making it more dinamic to fetch different menu

--- src/Pages/Home/PopularManu/PopularManu.jsx
+++ src/Pages/Home/PopularManu/PopularManu.jsx
@@ -1,26 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import Pagetitle from '../../../Components/Pagetitle';
 import MenuItem from '../../../Shared/MenuItem/MenuItem';
+import useMenu from '../../../hooks/useMenu';
 
 const PopularManu = () => {
 
-    const [menu, setMenu] = useState([])
+    const [menu] =  useMenu();
+    const popular  =  menu.filter(item => item.category ===  'popular')
+    // const [menu, setMenu] = useState([])
 
-    useEffect(() => {
-        fetch('menu.json')
-        .then( res => res.json())
-        .then(data => {
-            const popularItems =  data.filter( items => items.category === 'popular')
-            setMenu(popularItems);
-        })
-    },[])
+    // useEffect(() => {
+    //     fetch('menu.json')
+    //     .then( res => res.json())
+    //     .then(data => {
+    //         const popularItems =  data.filter( items => items.category === 'popular')
+    //         setMenu(popularItems);
+    //     })
+    // },[])
 
     return (
         <section className='mb-10'>
             <Pagetitle Heading="From our Menu" subHeading="Popular Iteams"></Pagetitle>
             <div className='grid md:grid-cols-2 gap-8'>
                 {
-                    menu.map(item => <MenuItem key={item._id} item={item}></MenuItem>)
+                    popular.map(item => <MenuItem key={item._id} item={item}></MenuItem>)
                 }
             </div>
         </section>
--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import Cover from '../../../Shared/Cover/Cover';
+import menuImage from '../../../assets/assets/menu/banner3.jpg'
+import PopularManu from '../../Home/PopularManu/PopularManu';
 
 const Menu = () => {
     return (
@@ -7,6 +10,17 @@ const Menu = () => {
             <Helmet>
                 <title>Bistro / Menu</title>
             </Helmet>
+            <Cover img={menuImage} title="our menu"></Cover>
+            <PopularManu></PopularManu>
+
+            <Cover img={menuImage} title="our menu"></Cover>
+            <PopularManu></PopularManu>
+
+            <Cover img={menuImage} title="our menu"></Cover>
+            <PopularManu></PopularManu>
+
+            <Cover img={menuImage} title="our menu"></Cover>
+            <PopularManu></PopularManu>
         </div>
     );
 };
